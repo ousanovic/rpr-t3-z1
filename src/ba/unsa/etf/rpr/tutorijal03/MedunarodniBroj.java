65153ba787116136a8556458c59772325c68f1f1
ousanovic <ousanovic1@etf.unsa.ba>
2019-09-02 23:30:50 +0200

    kreirana klasa MedunarodniBroj i implementirane potrebne metode

--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MedunarodniBroj {
+public class MedunarodniBroj extends TelefonskiBroj{
+    private String broj;
+
+    public MedunarodniBroj(String drzava, String broj) {
+        this.broj = drzava + broj;
+    }
+
+    @Override
+    public String ispisi() {
+        return broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return broj.hashCode();
+    }
 }
