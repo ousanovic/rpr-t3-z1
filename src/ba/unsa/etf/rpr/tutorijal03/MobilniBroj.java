c3bc91c91691ced8d31fba4b3cdca60187f49bd5
ousanovic <ousanovic1@etf.unsa.ba>
2019-09-02 23:28:50 +0200

    kreirana klasa MobilniBroj i implementirane potrebne metode

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class MobilniBroj extends TelefonskiBroj {
+    String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        this.broj = "0" + Integer.toString(mobilnaMreza) + "/" + broj;
+    }
+
+    @Override
+    public String ispisi() {
+        return broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return broj.hashCode();
+    }
 }
